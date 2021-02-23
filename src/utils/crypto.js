1b426374d1cc5a0a53f7e17b37ab753b1942521c
mablin7 <magyarb0201@gmail.com>
2021-02-23 13:53:16 +0100

    feat(store): add crypto wrappers

---
+++ src/utils/crypto.js
@@ -0,0 +1,28 @@
+import { NativeModules } from 'react-native'
+import { getUniqueId } from 'react-native-device-info'
+const { Aes } = NativeModules
+
+const PEPPER = '2iQ56n1oBs3agpagu7t3aqmwehZvLwKb3RYuH8xVjsYE5NJYJspgdawCmERfJg5DSnxqyDO'
+const COST = 5000
+const LENGTH = 256
+const IVLEN = 16
+
+export const getKey = (password, salt) => Aes.pbkdf2(password, salt+PEPPER, COST, LENGTH)
+
+export const decrypt = async ({ cipher, iv, hmac }, key) => {
+    const newHmac = await Aes.hmac256(cipher+iv, key)
+    if (newHmac !== hmac) throw 'Decryption failed'
+    const decrypted = await Aes.decrypt(cipher, key, iv)
+    return decrypted
+}
+
+export const encrypt = async (data, key) => {
+    const iv = await Aes.randomKey(IVLEN)
+    const cipher = await Aes.encrypt(data, key, iv)
+    const hmac = await Aes.hmac256(cipher+iv, key)
+    return { cipher, iv, hmac }
+}
+
+export const getUUID = () => Aes.randomUuid()
+
+export const getSalt = () => getUniqueId()
