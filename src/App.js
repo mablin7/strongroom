05fc915266c85a5ba7212a7059b5cae643447461
mablin7 <magyarb0201@gmail.com>
2021-03-09 23:19:04 +0100

    feat(unlock): add transition from unlock to vault

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+import { UIManager, LayoutAnimation } from 'react-native'
 
 import Unlock from './screens/Unlock'
 import Vault from './screens/Vault'
 import {DEFAULT_VAULT} from './utils/constants'
 
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
 export default () => {   
   const [vault, setVault] = useState(undefined)
+  const onVaultOpen = newVault => {
+    if (vault !== newVault) LayoutAnimation.configureNext(LayoutAnimation.Presets.linear)
+    setVault(newVault)
+  }
 
   return (
-    vault === undefined ? <Unlock vaultPath={DEFAULT_VAULT} onVaultOpen={setVault}/> : <Vault initialVault={vault}/>
+    vault === undefined ? <Unlock vaultPath={DEFAULT_VAULT} onVaultOpen={onVaultOpen}/> : <Vault initialVault={vault}/>
   )
 }
 
