9517751adbd09a9552a0d7aa2fdb631e53e17c81
mablin7 <magyarb0201@gmail.com>
2021-03-09 22:48:18 +0100

    feat(unlock): add actual unlock functionality

--- src/screens/Unlock.js
+++ src/screens/Unlock.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import { openVault } from '../vault'
 import { BG_COLOR } from '../utils/constants'
+import globalStyles from '../utils/styles'
 import { vaultExists } from '../utils/vaultFile'
 
+import PasswordInput from '../components/PasswordInput'
 import CreateVault from '../components/UnlockCreateVault'
 
 export default ({ vaultPath, onVaultOpen }) => {
@@ -14,26 +16,24 @@ export default ({ vaultPath, onVaultOpen }) => {
   }, [vaultPath])
 
   const onDone = async password => {
-    onVaultOpen(await openVault(vaultPath, password))
+    const vault = await openVault(vaultPath, password)
+    if (vault === undefined) return false
+    else onVaultOpen(vault)
   }
 
-  let screen
-  if (createNewVault === true) screen = <CreateVault onDone={onDone}/>
-  // TODO: unlock screen if vault exists
-  else if (createNewVault === false) screen = <Text>Unlock screen</Text>
-
-    return (
-      <View style={styles.container}>
-        {screen}
-      </View>
-    )
+  return (
+    <View style={styles.container}>
+      { createNewVault === true && <CreateVault onDone={onDone}/> }
+      { createNewVault === false && <PasswordInput onDone={onDone} btnTitle="Unlock!"/> }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: BG_COLOR,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    ...globalStyles.center,
+    flexDirection: 'column'
   }
 })
