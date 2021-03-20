23912219e97ea2716f944765616b2d9ca34059f3
mablin7 <magyarb0201@gmail.com>
2021-03-20 22:53:18 +0100

    feat: add SlideTransition to main screen + back handler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react'
-import { UIManager, LayoutAnimation } from 'react-native'
+import { useBackHandler } from '@react-native-community/hooks'
+
+import SlideTransition from './components/SlideTransition'
 
 import Unlock from './screens/Unlock'
 import Vault from './screens/Vault'
 import {DEFAULT_VAULT} from './utils/constants'
 
-if (Platform.OS === 'android') {
-  if (UIManager.setLayoutAnimationEnabledExperimental) {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
-}
-
 export default () => {   
   const [vault, setVault] = useState(undefined)
-  const onVaultOpen = newVault => {
-    if (vault !== newVault) LayoutAnimation.configureNext(LayoutAnimation.Presets.linear)
-    setVault(newVault)
-  }
+  useBackHandler(() => {
+    if (vault !== undefined) {
+      setVault(undefined)
+      return true
+    } else return false
+  })
 
   return (
-    vault === undefined ? <Unlock vaultPath={DEFAULT_VAULT} onVaultOpen={onVaultOpen}/> : <Vault initialVault={vault}/>
+    <SlideTransition goTo={vault === undefined ? 0 : 1} >
+      <Unlock vaultPath={DEFAULT_VAULT} onVaultOpen={setVault}/>
+      <Vault initialVault={vault}/>
+    </SlideTransition>
   )
 }
 
