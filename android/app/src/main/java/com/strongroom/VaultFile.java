37122c386bb1f8c341d0d439cbbbb33a518c5403
mablin7 <magyarb0201@gmail.com>
2021-04-26 16:33:23 +0200

    feat(android): add package for native crypto

--- android/app/src/main/java/com/strongroom/VaultFile.java
+++ android/app/src/main/java/com/strongroom/VaultFile.java
@@ -1,20 +1,62 @@
 package com.strongroom;
 
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collectors;
 
-public class VaultFileInternal {
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class VaultFile {
     String filePath;
+    SecretKey key;
 
-    public VaultFileInternal(String filePath, String key) {
+    InputStream decryptedStream;
+
+    public VaultFile(String filePath, SecretKey key) {
         this.filePath = filePath;
+        this.key = key;
     }
 
-    public OutputStream getDecryptedStream() {
+    public InputStream getDecryptedStream()
+            throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException
+    {
+        if (decryptedStream == null) {
+            try (FileInputStream fis = new FileInputStream(filePath)) {
+                Cipher cipher = Cipher.getInstance(VaultManager.CIPHER_ALGORITHM);
+                cipher.init(Cipher.DECRYPT_MODE, key);
+                decryptedStream = new CipherInputStream(fis, cipher);
+            } catch (IOException e) {
+                throw e;
+            }
+        }
 
+        return decryptedStream;
     }
 
-    public String getPlainTextContents() {
+    public String getPlainTextContents()
+            throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException
+    {
+        getDecryptedStream();
 
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        for (int length; (length = decryptedStream.read(buffer)) != -1; ) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString("UTF-8");
     }
 
     public void getInputStream() {
--- android/app/src/main/java/com/strongroom/VaultManager.java
+++ android/app/src/main/java/com/strongroom/VaultManager.java
@@ -1,4 +1,82 @@
 package com.strongroom;
+import android.util.Log;
 
-public class VaultManager {
+import com.facebook.common.util.Hex;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class VaultManager extends ReactContextBaseJavaModule {
+
+    public static final String KEY_ALGORITHM = "AES";
+    public static final String KEYGEN_ALGORITHM = "PBKDF2WithHmacSHA512";
+    public static final String CIPHER_ALGORITHM = "AES/CBC/PKCS7Padding";
+
+    private static final String ERROR_PBEKEYGEN = "ERR_PBEKEYGEN";
+
+    VaultManager(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "VaultManager";
+    }
+
+    public static SecretKey getKeyFromString(String hexKey) {
+        byte[] key = Hex.decodeHex(hexKey);
+        SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
+        return  secretKey;
+    }
+
+    @ReactMethod
+    public void deriveKey(String password, String salt, Integer cost, Integer length, Promise promise) {
+        try {
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes("UTF_8"), cost, length);
+            SecretKey key = SecretKeyFactory.getInstance(KEYGEN_ALGORITHM).generateSecret(spec);
+            byte[] encodedKey = key.getEncoded();
+            promise.resolve(Hex.encodeHex(encodedKey, false));
+        } catch (Exception e) {
+            promise.reject(ERROR_PBEKEYGEN, e.getMessage());
+        }
+    }
+
+    @ReactMethod
+    public void test() {
+        try {
+            String t = "test", s = "salt";
+            int cost = 1000, len = 256;
+            PBEKeySpec spec = new PBEKeySpec(t.toCharArray(), s.getBytes("UTF_8"), cost, len);
+            SecretKey key = SecretKeyFactory.getInstance(KEYGEN_ALGORITHM).generateSecret(spec);
+            byte[] encodedKey = key.getEncoded();
+            String hexKey = Hex.encodeHex(encodedKey, false);
+
+            byte[] keydec = Hex.decodeHex(hexKey);
+            SecretKey secretKey = new SecretKeySpec(keydec, KEY_ALGORITHM);
+
+            Log.d("VaultManager", "Key derivation works: " + secretKey.equals(key) + "\n" + secretKey.toString() + " " + key.toString());
+        } catch (Exception e) {
+
+        }
+    }
+
+//    @ReactMethod
+//    public String getManifest(String vaultPath, String key, Promise promise) {
+//
+//    }
+//
+//    @ReactMethod
+//    public void updateManifest(String vaultPath, String key, String newManifest, Promise promise) {
+//
+//    }
 }
